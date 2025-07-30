aa7e4c6760bda4ea26a94076641d78febc516a3a
Himanshuu <2203himanshusingh@gmail.com>
2025-07-30 12:25:43 +0530

    dfas

--- src/components/bottom.jsx
+++ src/components/bottom.jsx
@@ -5,17 +5,7 @@ import "../styles/bottom.css"; // Importing the CSS file
 function Bottom() {
   return (
     <div className="bottom">
-      <div className="bottom-links">
-        <Nav.Link href="/" className="bottom-link">
-          Home
-        </Nav.Link>
-        <Nav.Link href="/aboutUs" className="bottom-link">
-          About
-        </Nav.Link>
-        <Nav.Link href="/contact" className="bottom-link">
-          Contact
-        </Nav.Link>
-      </div>
+      <div className="bottom-links"></div>
 
       <div className="bottom-about">
         <div className="bottom-about-title">Why do hire me?</div>
